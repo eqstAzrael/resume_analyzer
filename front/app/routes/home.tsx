67b37b17fcbee270c22242458454a2f4e29ec3a2
eqstAzrael <bazirashvili@gmail.com>
2026-01-09 20:49:40 +0300

    real resumes

--- front/app/routes/home.tsx
+++ front/app/routes/home.tsx
@@ -1,9 +1,8 @@
 import type { Route } from "./+types/home";
 import Navbar from "../components/Navbar";
 import ResumeCard from "~/components/ResumeCard";
-import {resumes} from "../constants";
-import { useEffect } from "react";
-import { useLocation, useNavigate } from "react-router";
+import { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router";
 import { usePuterStore } from "~/lib/puter";
 
 
@@ -15,31 +14,71 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-    const { isLoading, auth } = usePuterStore();
+  const { auth, kv} = usePuterStore();    
+  const navigate = useNavigate()
+  const [resumes, setResumes] = useState<Resume[]>([]);
+  const [loadingResumes, setLoadingResumes] = useState(false);
 
-    
-    const navigate = useNavigate()
-
-    useEffect(() => {
+  useEffect(() => {
         if(!auth.isAuthenticated) navigate('/auth?next=/'); 
-    }, [auth.isAuthenticated])
+  }, [auth.isAuthenticated])
+
+  useEffect(() => {
+    const loadResumes = async () => {
+      setLoadingResumes(true);
+
+      const resumes = (await kv.list('resume:*', true)) as KVItem[];
+      const parsedResumes = resumes?.map((resume) => (
+        JSON.parse(resume.value) as Resume
+      ))
+      console.log("parsedResumes", parsedResumes);
+      setResumes(parsedResumes || []);
+      setLoadingResumes(false);
+    }
+
+    loadResumes();
+  }, []);
 
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar/>
+
     <section className="main-section">
       <div className="page-heading">
         <h1> Track Your Applications & Resume Ratings</h1>
-        <h2> Review your submissions and check AI-powered feedback </h2>
+
+        {!loadingResumes && resumes?.length === 0 ? (
+          <h2> No resumes found. Upload your first resume to get feedback</h2>
+        ): (
+          <h2> Review your submissions and check AI-powered feedback </h2>
+        )}
+        
       </div>
     
+      {loadingResumes && (
+        <div className="flex flex-col items-center justify-center">
+          <img src="/images/resume-scan-2.gif" className="w-[200px]" />
+        </div>
+      )}
 
-      {resumes.length > 0 && (
+      {!loadingResumes && resumes.length > 0 && (
         <div className="resumes-section">
           {resumes.map((resume) => (
             <ResumeCard key={resume.id} resume={resume} />
           ))}
         </div>
       )}
+
+      {!loadingResumes && resumes?.length === 0 && (
+        <div className="flex flex-col items-center justify-center mt-10 gap-4">
+          <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
+            Upload Resume
+          </Link>
+        </div>
+      )}
     </section>
   </main>
 }
+function setResumeUrl(url: string) {
+  throw new Error("Function not implemented.");
+}
+
